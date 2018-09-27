33bcf7fc618aaf8fef91020b8f6f85556e78eefa
Rounak Sengupta <42744463+MrReese0@users.noreply.github.com>
2018-09-26 20:23:48 -0700

    Update 3Sum.py

--- 3Sum.py
+++ 3Sum.py
@@ -4,28 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        newarr = []
+        res = []
         nums.sort()
-        start = 0
-        end = len(nums)-1
-        newarr = []
-        print(nums)
-        while start < end:
-            keyvalue = nums[start] + nums[end]
-            print("key value " + str(keyvalue))
-            print(nums[start+1:end])
-            if -keyvalue in nums[start+1:end]:
-                print("nums start " + str(nums[start]))
-                print("nums -keyvalue " + str(nums.index(-keyvalue)))
-                print("nums end " + str(nums[end]))
-                if [nums[start],nums[nums.index(-keyvalue)],nums[end]] not in newarr:
-                    newarr.append([nums[start],nums[nums.index(-keyvalue)],nums[end]])
-                print("new array" + str(newarr))
-            if keyvalue < 0:
-                start += 1
-                print("nums start " + str(nums[start]))
-            else:
-                end -= 1
-                print("nums end " + str(nums[end]))
-
-        return newarr
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            start = i+1
+            end = len(nums)-1
+            while start < end:
+                s = nums[i] + nums[start] + nums[end]
+                if s < 0:
+                    start +=1 
+                elif s > 0:
+                    end -= 1
+                else:
+                    res.append((nums[i], nums[start], nums[end]))
+                    while start < end and nums[start] == nums[start+1]:
+                        start += 1
+                    while start < end and nums[end] == nums[end-1]:
+                        end -= 1
+                    start += 1; end -= 1
+        return res
